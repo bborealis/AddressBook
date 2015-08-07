953dd416b2336d915f635bffaf49e95b04bfbbcd
brian <brian.borealis@gmail.com>
2015-08-07 10:16:08 -0700

    added app.php file

--- app/app.php
+++ app/app.php
@@ -4,5 +4,34 @@
 
     session_start();
 
+    if (empty($_SESSION['contact_list'])) {
+        $_SESSION['contact_list'] = array();
+    }
 
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('contact.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->get("/contacts", function() use ($app) {
+        return $app['twig']->render('new_contact.html.twig');
+    });
+
+    $app->post("/", function() use ($app) {
+        $contact = new Contact($_POST['name'], $_POST['phoneNumber'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('contact.html.twig', array('contacts' => Contact::getAll()));
+    });
+
+    $app->post("delete_contact", function() use ($app) {
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contact.html.twig');
+    });
+
+    return $app;
 ?>
